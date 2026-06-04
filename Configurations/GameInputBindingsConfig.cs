f8a7a2a9712d4023290ef3b163adea9150cdbda0
SacraneScrny <a.spas.gvnm@gmail.com>
2026-06-05 04:38:10 +0500

    upd

--- Configurations/GameInputBindingsConfig.cs
+++ Configurations/GameInputBindingsConfig.cs
@@ -1,4 +1,4 @@
-﻿using Sackrany.ConfigSystem.SackranyConfig;
+﻿using SackranyConfig;
 
 namespace SackranyInput.Configurations
 {
--- InputManager.cs
+++ InputManager.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 
-using Sackrany.ConfigSystem.SackranyConfig;
+using SackranyConfig;
 
 using SackranyInput.Caches;
 using SackranyInput.Configurations;
